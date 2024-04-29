f52348087462933a17d5fe1c1c31ad9e0944a6e7
zoom32 <itclasszoom32@mail.ru>
2024-04-29 17:23:44 +0300

    v1.0

---
+++ exm1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//standart input output
+#include<conio.h>//console input output
+#include<locale.h>
+
+main()
+{
+	setlocale(LC_ALL,"rus");
+    printf("the first line\n");
+    
+	getch();
+}
